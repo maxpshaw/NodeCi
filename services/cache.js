fb47c5473d86338e2d0b4a09e95fc7c89e2a2773
Max Shaw <maxpshaw@gmail.com>
2018-07-30 15:16:40 -0700

    Complete section4#62

--- services/cache.js
+++ services/cache.js
@@ -7,8 +7,19 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function() {
+  // *this* = the query
+  this.useCache = true;
+  // make this function chainable
+  return this;
+};
+
 // dont use arrow b/c it will mess up *this*
 mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+
   const key = JSON.stringify(
     Object.assign({}, this.getQuery(), {
       collection: this.mongooseCollection.name
