dcca742b061a04175cf5605ca9169cf140ab917c
Max Shaw <maxpshaw@gmail.com>
2018-07-30 14:49:09 -0700

    Comlete section4#58

--- services/cache.js
+++ services/cache.js
@@ -8,19 +8,23 @@ client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
 // dont use arrow b/c it will mess up *this*
-mongoose.Query.prototype.exec = function() {
-  console.log('IM ABOUT TO RUN A QUERY');
+mongoose.Query.prototype.exec = async function() {
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
 
-  // modifying getQuery will affect the query
-  //   console.log(this.getQuery());
-  //   console.log(this.mongooseCollection.name);
+  // See if we have a value for 'key' in redis
+  // dont forget async keyword
+  const cacheValue = await client.get(key);
 
-  // copy properties from one to another
-  const key = Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name
-  });
+  // If we do return that
+  if (cacheValue) {
+    console.log(cacheValue);
+  }
 
-  console.log(key);
-
-  return exec.apply(this, arguments);
+  // Otherwise, issue the query and store the result in redis
+  const result = await exec.apply(this, arguments);
+  console.log(result);
 };
