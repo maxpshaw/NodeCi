a8e7f5df6014c4826e88141fc2d57de671f89885
Max Shaw <maxpshaw@gmail.com>
2018-08-01 15:47:59 -0700

    section5#75

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,18 @@
+const puppeteer = require('puppeteer');
+
 test('Add two numbers', () => {
   const sum = 1 + 2;
   expect(sum).toEqual(3);
+});
+
+test('We can launch a browser', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: '/usr/bin/chromium'
+  });
+  const page = await browser.newPage();
+  await page.goto('localhost:3000');
+
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  expect(text).toEqual('Blogster');
 });
