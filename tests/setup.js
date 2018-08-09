edf7092d66d9e26aba27d75327f3443ea195880b
Max Shaw <maxpshaw@gmail.com>
2018-08-09 05:09:55 -0700

    section5#93

---
+++ tests/setup.js
@@ -0,0 +1,10 @@
+require('../models/User');
+
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+mongoose.Promise = global.Promise; //by detault, it doesnt use its default implementation
+mongoose.connect(
+  keys.mongoURI,
+  { useMongoClient: true }
+); //recording keeping to config momgoose properly to await deprecation warning
