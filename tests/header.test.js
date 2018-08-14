2c19e9768fad2ddc4a9ea70b7cb9eabf28b79500
Max Shaw <maxpshaw@gmail.com>
2018-08-13 22:15:20 -0700

    section5#101

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,21 +1,16 @@
-const puppeteer = require('puppeteer');
 const sessionFactory = require('./factories/sessionFactory');
 const userFactory = require('./factories/userFactory');
+const Page = require('./helpers/page');
 
-let browser;
 let page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false,
-    executablePath: '/usr/bin/chromium'
-  });
-  page = await browser.newPage();
+  page = await Page.build();
   await page.goto('localhost:3000');
 });
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
 
 test('the header has the correct text', async () => {
